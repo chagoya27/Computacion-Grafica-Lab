6b8d2ecc34b18cc6874e05bf446acbf3869d37fb
chagoya27 <leocingon@gmail.com>
2025-08-23 20:57:50 -0600

    agrego práctica2

--- practicas/Practica2/Practica2/Main_Dibujo2D.cpp
+++ practicas/Practica2/Practica2/Main_Dibujo2D.cpp
@@ -1,3 +1,9 @@
+// Practica 2
+// Leonardo Chagoya Gonzalez
+// 23 de agosto de 2025
+// 318218814
+
+
 #include<iostream>
 
 //#define GLEW_STATIC
@@ -8,10 +14,11 @@
 
 // Shaders
 #include "Shader.h"
+#include <vector>
 
 void resize(GLFWwindow* window, int width, int height);
 
-const GLint WIDTH = 800, HEIGHT = 600;
+const GLint WIDTH = 800, HEIGHT = 600, pixelSize = 20;
 
 
 int main() {
@@ -23,7 +30,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);*/
 
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Dibujo de Primitivas en 2D, dibujo de triangulos con indices", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Practica 2. Leonardo Chagoya", NULL, NULL);
 	glfwSetFramebufferSizeCallback(window, resize);
 	
 	//Verificaci�n de errores de creacion  ventana
@@ -60,14 +67,225 @@ int main() {
 	Shader ourShader("Shader/core.vs", "Shader/core.frag");
 
 
-	// Set up vertex data (and buffer(s)) and attribute pointers
 	float vertices[] = {
-		// Posiciones          // Colores en RGB
-		0.5f,  0.5f, 0.0f,    0.5f,0.7f,1.0f,  // top right
-		0.5f, -0.5f, 0.0f,    1.0f,1.0f,0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,   1.0f,0.0f,1.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f,   1.0f,1.0f,0.0f, // top left 
+		//Cuerpo del Boo
+		// Rectángulo 1
+		-0.8f, -0.4f, 0.0f,  1.0f, 1.0f, 1.0f,  // Abajo izquierda
+		-0.8f, -0.1f, 0.0f,  1.0f, 1.0f, 1.0f,  // Arriba izquierda
+		-0.7f, -0.4f, 0.0f,  1.0f, 1.0f, 1.0f,  // Abajo derecha
+		-0.7f, -0.1f, 0.0f,  1.0f, 1.0f, 1.0f,  // Arriba derecha
+
+		// Rectángulo 2
+		-0.7f, -0.5f, 0.0f,  1.0f, 1.0f, 1.0f,
+		-0.7f,  0.2f, 0.0f,  1.0f, 1.0f, 1.0f,  
+		-0.2f, -0.5f, 0.0f,  1.0f, 1.0f, 1.0f, 
+		-0.2f,  0.2f, 0.0f,  1.0f, 1.0f, 1.0f,  
+
+		// Rectangulo 3
+		-0.6f, 0.2f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.6f, 0.4f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.2f, 0.2f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.2f, 0.4f, 0.0f,	1.0f,1.0f,1.0f,
+
+		// Rectangulo 4
+		-0.5f, 0.4f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.5f, 0.5f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.2f, 0.4f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.2f, 0.5f, 0.0f,	1.0f,1.0f,1.0f,
+
+
+		// Rectangulo 5
+		-0.3f, 0.5f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.3f, 0.6f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.2f, 0.5f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.2f, 0.6f, 0.0f,	1.0f,1.0f,1.0f,
+
+
+		// Rectangulo 6
+		-0.4f, -0.6f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.4f, -0.5f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.2f, -0.6f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.2f, -0.5f, 0.0f,	1.0f,1.0f,1.0f,
+
+
+		// Rectangulo 7
+		-0.2f, -0.7f, 0.0f,	1.0f,1.0f,1.0f,
+		-0.2f, 0.6f, 0.0f,	1.0f,1.0f,1.0f,
+		0.2f, -0.7f, 0.0f,	1.0f,1.0f,1.0f,
+		0.2f, 0.6f, 0.0f,	1.0f,1.0f,1.0f,
+
+
+		// Rectangulo 8
+		0.2f, -0.6f, 0.0f,	1.0f,1.0f,1.0f,
+		0.2f, 0.5f, 0.0f,	1.0f,1.0f,1.0f,
+		0.5f, -0.6f, 0.0f,	1.0f,1.0f,1.0f,
+		0.5f, 0.5f, 0.0f,	1.0f,1.0f,1.0f,
+
+
+		// Rectangulo 9
+		0.2f, -0.6f, 0.0f,	1.0f,1.0f,1.0f,
+		0.2f, 0.5f, 0.0f,	1.0f,1.0f,1.0f,
+		0.5f, -0.6f, 0.0f,	1.0f,1.0f,1.0f,
+		0.5f, 0.5f, 0.0f,	1.0f,1.0f,1.0f,
+
+		
+		// Rectangulo 10
+		0.2f, 0.5f, 0.0f,	1.0f,1.0f,1.0f,
+		0.2f, 0.6f, 0.0f,	1.0f,1.0f,1.0f,
+		0.3f, 0.5f, 0.0f,	1.0f,1.0f,1.0f,
+		0.3f, 0.6f, 0.0f,	1.0f,1.0f,1.0f,
+
+
+		// Rectangulo 10
+		0.5f, -0.5f, 0.0f,	1.0f,1.0f,1.0f,
+		0.5f, -0.3f, 0.0f,	1.0f,1.0f,1.0f,
+		0.6f, -0.5f, 0.0f,	1.0f,1.0f,1.0f,
+		0.6f, -0.3f, 0.0f,	1.0f,1.0f,1.0f,
+
+
+		// Rectangulo 11
+		0.5f, -0.3f, 0.0f,	1.0f,1.0f,1.0f,
+		0.5f, 0.2f, 0.0f,	1.0f,1.0f,1.0f,
+		0.7f, -0.3f, 0.0f,	1.0f,1.0f,1.0f,
+		0.7f, 0.2f, 0.0f,	1.0f,1.0f,1.0f,
+
+
+		// Rectangulo 12
+		0.5f, 0.2f, 0.0f,	1.0f,1.0f,1.0f,
+		0.5f, 0.4f, 0.0f,	1.0f,1.0f,1.0f,
+		0.6f, 0.2f, 0.0f,	1.0f,1.0f,1.0f,
+		0.6f, 0.4f, 0.0f,	1.0f,1.0f,1.0f,
+
+
+		//sombras
+
+		//rectangulo 13
+		-0.2f, -0.7f, 0.0f,	0.5f,0.5f,0.5f,
+		-0.2f, -0.6f, 0.0f,	0.5f,0.5f,0.5f,
+		0.2f, -0.7f, 0.0f,	0.5f,0.5f,0.5f,
+		0.2f, -0.6f, 0.0f,	0.5f,0.5f,0.5f,
+
+
+		//rectangulo 14
+		-0.4f, -0.6f, 0.0f,	0.5f,0.5,0.5f,
+		-0.4f, -0.5f, 0.0f,	0.5f,0.5f,0.5f,
+		-0.1f, -0.6f, 0.0f,	0.5f,0.5f,0.5f,
+		-0.1f, -0.5f, 0.0f,	0.5f,0.5f,0.5f,
+
+
+		//rectangulo 15
+		-0.7f, -0.5f, 0.0f, 0.5f, 0.5, 0.5f,
+		-0.7f, -0.4f, 0.0f, 0.5f, 0.5, 0.5f,
+		-0.3f, -0.5f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.3f, -0.4f, 0.0f, 0.5f, 0.5f, 0.5f,
+
+		//rectangulo 16
+		-0.8f, -0.4f, 0.0f, 0.5f, 0.5, 0.5f,
+		-0.8f, -0.3f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.6f, -0.4f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.6f, -0.3f, 0.0f, 0.5f, 0.5f, 0.5f,
+
+
+		//rectangulo 17
+		-0.8f, -0.3f, 0.0f, 0.5f, 0.5, 0.5f,
+		-0.8f, -0.1f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.7f, -0.3f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.7f, -0.1f, 0.0f, 0.5f, 0.5f, 0.5f,
+
+		//rectangulo 18
+		-0.7f, -0.1f, 0.0f, 0.5f, 0.5, 0.5f,
+		-0.7f, 0.2f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.6f, -0.1f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.6f, 0.2f, 0.0f, 0.5f, 0.5f, 0.5f,
+
+		//rectangulo 19
+		-0.6f, 0.2f, 0.0f, 0.5f, 0.5, 0.5f,
+		-0.6f, 0.4f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.5f, 0.2f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.5f, 0.4f, 0.0f, 0.5f, 0.5f, 0.5f,
+
+		//rectangulo 20
+		-0.5f, 0.4f, 0.0f, 0.5f, 0.5, 0.5f,
+		-0.5f, 0.5f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.3f, 0.4f, 0.0f, 0.5f, 0.5f, 0.5f,
+		-0.3f, 0.5f, 0.0f, 0.5f, 0.5f, 0.5f,
+
+
+		// <<<<<<OJOS>>>>>>>
+		//rectangulo 21
+		0.2f, 0.1f, 0.0f, 0.0f, 0.0F, 0.0f,
+		0.2f, 0.3f, 0.0f, 0.0f, 0.0f, 0.0f,
+		0.3f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f,
+		0.3f, 0.3f, 0.0f, 0.0f, 0.0f, 0.0f,
+
+		//rectangulo 22
+		0.4f, 0.1f, 0.0f, 0.0f, 0.0F, 0.0f,
+		0.4f, 0.3f, 0.0f, 0.0f, 0.0f, 0.0f,
+		0.5f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f,
+		0.5f, 0.3f, 0.0f, 0.0f, 0.0f, 0.0f,
+
+
+		// <<<<<<<BOCA>>>>>>>
+		0.0f, -0.5f, 0.0f, 1.0f, 0.0F, 0.0f,
+		0.0f, -0.3f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.1f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.1f, -0.3f, 0.0f, 1.0f, 0.0f, 0.0f,
+
+		0.1f, -0.4f, 0.0f, 1.0f, 0.0F, 0.0f,
+		0.1f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.2f, -0.4f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.2f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+
+
+		0.2f, -0.5f, 0.0f, 1.0f, 0.0F, 0.0f,
+		0.2f, -0.1f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.3f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.3f, -0.1f, 0.0f, 1.0f, 0.0f, 0.0f,
+
+		0.3f, -0.4f, 0.0f, 1.0f, 0.0F, 0.0f,
+		0.3f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.4f, -0.4f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.4f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+
+		0.4f, -0.5f, 0.0f, 1.0f, 0.0F, 0.0f,
+		0.4f, -0.1f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.1f, 0.0f, 1.0f, 0.0f, 0.0f,
+
+
+		0.5f, -0.3f, 0.0f, 1.0f, 0.0F, 0.0f,
+		0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.6f, -0.3f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.6f, -0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+
+
+		// <<<<<<<<DETALLE>>>>>>>>>>
+
+		-0.5f, -0.2f, 0.0f,	0.0f, 0.0F, 0.0f,
+		-0.5f, -0.1f, 0.0f,	0.0f, 0.0f, 0.0f,
+		-0.4f, -0.2f, 0.0f,	0.0f, 0.0f, 0.0f,
+		-0.4f, -0.1f, 0.0f,	0.0f, 0.0f, 0.0f,
+
+		-0.6f, -0.1f, 0.0f, 0.0f, 0.0F, 0.0f,
+		-0.6f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f,
+		-0.5f, -0.1f, 0.0f, 0.0f, 0.0f, 0.0f,
+		-0.5f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f,
+
+
+		-0.5f, 0.1f, 0.0f, 0.0f, 0.0F, 0.0f,
+		-0.5f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f,
+		-0.3f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f,
+		-0.3f, 0.2f, 0.0f, 0.0f, 0.0f, 0.0f,
+
+		-0.3f, 0.0f, 0.0f, 0.0f, 0.0F, 0.0f,
+		-0.3f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f,
+		-0.2f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+		-0.2f, 0.1f, 0.0f, 0.0f, 0.0f, 0.0f,
+
+
+
 	};
+
 
 	//indices de los vertices
 	unsigned int indices[] = {  // note that we start from 0!
@@ -75,6 +293,11 @@ int main() {
 		0,1,3,
 		
 	};
+
+
+
+
+
 
 
 	// Generamos los buffers y los enlazamos
@@ -87,8 +310,12 @@ int main() {
 	glBindVertexArray(VAO);
 
 	//2.- Copiamos nuestros arreglo de vertices en un buffer de vertices para que OpenGL lo use
+	/*glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);*/
+	
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	// 3.Copiamos nuestro arreglo de indices en  un elemento del buffer para que OpenGL lo use
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
@@ -109,6 +336,9 @@ int main() {
 	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
 
 
+
+
+
 	// Se dibuja en frames por segundo
 	while (!glfwWindowShouldClose(window))
 	{
@@ -122,22 +352,32 @@ int main() {
 
 
 		// Draw our first triangle
-        ourShader.Use();
-        glBindVertexArray(VAO);
+		ourShader.Use();
+		glBindVertexArray(VAO);
 
 
 		//dibujo de puntos/vertices
-        //glPointSize(10);
-        //glDrawArrays(GL_POINTS,0,4);
-        
+		glPointSize(pixelSize);
+
+		//glDrawArrays(GL_TRIANGLES, 0, 12); // 12 vértices (2 rectángulos × 6 vértices)
+
+
+
+		//dibujar todos los rectangulos
+		float i = 0;
+		for (i = 0; i < sizeof(vertices); i = i + 4)
+		{
+			glDrawArrays(GL_TRIANGLE_STRIP, i, 4);
+		}
 
 		//dibujo de lineas
+		//glLineWidth(10.0f);
 		//glDrawArrays(GL_LINES,0,2); //apartir del 0 hasta el vertice 2, las dos primeras lineas
         //glDrawArrays(GL_LINE_LOOP,0,4); //ciera todos los vertices
         
 		//dibujo de triangulos
-        //glDrawArrays(GL_TRIANGLES,2,4);
-        glDrawElements(GL_TRIANGLES, 3,GL_UNSIGNED_INT,0);
+		//glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        //glDrawElements(GL_TRIANGLES, 3,GL_UNSIGNED_INT,0);
 
         
         
